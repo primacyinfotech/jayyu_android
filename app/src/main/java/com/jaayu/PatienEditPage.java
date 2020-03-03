05bacdd98bd84b9708d2250cff1b4db948cce884
Administrator <kabir@primacyinfotech.com>
2020-03-03 15:27:31 +0530

    Patient Module

---
+++ app/src/main/java/com/jaayu/PatienEditPage.java
@@ -0,0 +1,14 @@
+package com.jaayu;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PatienEditPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_patien_edit_page);
+    }
+}
---
+++ app/src/main/java/com/jaayu/PatientAddPage.java
@@ -0,0 +1,14 @@
+package com.jaayu;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PatientAddPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_patient_add_page);
+    }
+}
