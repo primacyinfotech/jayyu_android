9ac5afe997f68f86f17278bcc33855e81b2e1f93
Administrator <kabir@primacyinfotech.com>
2020-03-17 18:39:15 +0530

    Account Prescription Module

--- app/src/main/java/Adapter/AccountPresPatientAdapter.java
+++ app/src/main/java/Adapter/AccountPresPatientAdapter.java
@@ -39,7 +39,8 @@ public class AccountPresPatientAdapter extends RecyclerView.Adapter<AccountPresP
     @Override
     public void onBindViewHolder(@NonNull AccountPresPatientAdapter.MyViewHolder holder, int position) {
         final AccountPresPatientModel mList = accountPresPatientModels.get(position);
-        
+
+
 
     }
 
