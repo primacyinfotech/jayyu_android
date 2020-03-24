d8ac12d152b55f9d7b5ddb0fcd3f0e20ab011e4d
SOMNATH <pad0015>
2020-03-24 10:14:53 +0530

    reminder

--- app/src/main/java/com/jaayu/Model/BaseUrl.java
+++ app/src/main/java/com/jaayu/Model/BaseUrl.java
@@ -1,6 +1,6 @@
 package com.jaayu.Model;
 
 public class BaseUrl {
-    public static String BaseUrlNew = "http://work.primacyinfotech.com/jaayu/api/";
+    public static String BaseUrlNew = "https://work.primacyinfotech.com/jaayu/api/";
 
 }
