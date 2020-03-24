5fffc3a31c954f25dcc128fdc19d67f5b54127b3
SOMNATH <pad0015>
2020-03-24 15:25:09 +0530

    reminder

--- app/src/main/java/com/jaayu/ReminderAddPage.java
+++ app/src/main/java/com/jaayu/ReminderAddPage.java
@@ -1,11 +1,20 @@
 package com.jaayu;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.CheckBox;
+import android.widget.LinearLayout;
+import android.widget.RadioButton;
+import android.widget.Spinner;
 
 public class ReminderAddPage extends AppCompatActivity {
-
+ private Spinner edit_medicine,days_count,month_date;
+ RadioButton daily_time,until_stop,weekly_time,monthly_time;
+ LinearLayout section_daily_time,section_of_week,section_of_month,new_time;
+ private CheckBox sunday,monday,tuesday,wednessday,thirstday,friday,saturday;
+ RecyclerView  timer_list;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
