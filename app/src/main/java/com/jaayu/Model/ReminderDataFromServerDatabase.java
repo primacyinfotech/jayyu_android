4a11995ec59c46b71623c70fc4d97bf29d23b9e0
SOMNATH <pad0015>
2020-03-27 16:51:28 +0530

    reminder

--- app/src/main/java/com/jaayu/Model/ReminderDataFromServerDatabase.java
+++ app/src/main/java/com/jaayu/Model/ReminderDataFromServerDatabase.java
@@ -11,6 +11,7 @@ import androidx.annotation.Nullable;
 public class ReminderDataFromServerDatabase extends SQLiteOpenHelper {
     public static final String DATABASE_NAME = "ReminderData.db";
     public static final String TABLE_NAME = "reminder_table";
+    public static final String TABLE_TIME_NAME = "time_table";
     public static final String COL_1 = "medicine_name";
     public static final String COL_2 = "user_id";
     public static final String COL_3 = "repeat_ed";
@@ -25,7 +26,7 @@ public class ReminderDataFromServerDatabase extends SQLiteOpenHelper {
     public static final String COL_12 = "start_date";
     public static final String COL_13 = "end_date";
     public static final String COL_14 = "duration";
-    public static final String COL_15 = "set_time";
+    public static final String COL_TIME = "set_time";
 
 
 
@@ -37,12 +38,14 @@ public class ReminderDataFromServerDatabase extends SQLiteOpenHelper {
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
         sqLiteDatabase.execSQL("create table " + TABLE_NAME +" (ID INTEGER PRIMARY KEY AUTOINCREMENT,medicine_name TEXT," +
                 "user_id TEXT,repeat_ed TEXT,sun TEXT,mon TEXT,tue TEXT,wed TEXT,thu TEXT,fri TEXT,sat TEXT,month_date TEXT," +
-                "start_date TEXT,end_date TEXT,duration TEXT,set_time TEXT)");
+                "start_date TEXT,end_date TEXT,duration TEXT)");
+        sqLiteDatabase.execSQL("create table " + TABLE_TIME_NAME +" (ID INTEGER PRIMARY KEY AUTOINCREMENT,set_time TEXT)");
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
         sqLiteDatabase.execSQL("DROP TABLE IF EXISTS "+TABLE_NAME);
+        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS "+TABLE_TIME_NAME);
         onCreate(sqLiteDatabase);
     }
     public boolean insertData(String medicine_name,String user_id,String repeat,String sunday,String monday,String tuesday,String wednesday,
@@ -72,9 +75,9 @@ public class ReminderDataFromServerDatabase extends SQLiteOpenHelper {
     public boolean insertTime(String setTheTime) {
         SQLiteDatabase db2 = this.getWritableDatabase();
         ContentValues contentValues2 = new ContentValues();
-        contentValues2.put(COL_15,setTheTime);
+        contentValues2.put(COL_TIME,setTheTime);
 
-        long result2 = db2.insert(TABLE_NAME,null ,contentValues2);
+        long result2 = db2.insert(TABLE_TIME_NAME,null ,contentValues2);
         if(result2 == -1)
             return false;
         else
@@ -83,6 +86,11 @@ public class ReminderDataFromServerDatabase extends SQLiteOpenHelper {
     public Cursor getAllData() {
         SQLiteDatabase db = this.getWritableDatabase();
         Cursor res = db.rawQuery("select * from "+TABLE_NAME,null);
+        return res;
+    }
+    public Cursor getAllTME() {
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor res = db.rawQuery("select * from "+TABLE_TIME_NAME,null);
         return res;
     }
 }
